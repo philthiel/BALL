3d9fbf7a34391cb1591029d74446c62bf03efc90
pthiel <thiel@informatik.uni-tuebingen.de>
2016-07-04 13:30:19 +0200

    Modified header and source collector macros

--- cmake/ball_functions.cmake
+++ cmake/ball_functions.cmake
@@ -16,14 +16,26 @@ endmacro()
 
 
 # --------------------------------------------------------------------------
+# Macros to set and reset CXX compiler flags
+#
+macro(BALL_MACRO_cxx_flags_set FLAGS)
+	set(BACKUP_CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE})
+	set(CMAKE_CXX_FLAGS_RELEASE ${FLAGS})
+endmacro()
+
+macro(BALL_MACRO_cxx_flags_reset)
+	set(CMAKE_CXX_FLAGS_RELEASE ${BACKUP_CMAKE_CXX_FLAGS_RELEASE})
+endmacro()
+
+
+# --------------------------------------------------------------------------
 # Macro to collect  header files (*.h and *.iC) from include directory
 #
-macro(BALL_MACRO_collect_headers WORKING_DIR LIST_NAME)
+macro(BALL_MACRO_collect_headers HEADER_FILE_LIST)
 
 	# Step 1: collect groups
 	set(GROUPS)
-	file(GLOB_RECURSE ENTRIES RELATIVE ${WORKING_DIR}/include/BALL "${WORKING_DIR}/include/BALL/*")
-
+	file(GLOB_RECURSE ENTRIES RELATIVE ${PROJECT_SOURCE_DIR}/include/BALL "include/BALL/*")
 	foreach(e ${ENTRIES})
 		get_filename_component(group ${e} DIRECTORY)
 		list(APPEND GROUPS ${group})
@@ -32,7 +44,7 @@ macro(BALL_MACRO_collect_headers WORKING_DIR LIST_NAME)
 
 	# Step 2: collect files per group and add to list
 	foreach(g ${GROUPS})
-		file(GLOB_RECURSE FILES RELATIVE ${WORKING_DIR} "${WORKING_DIR}/include/BALL/${g}/*.h" "/${WORKING_DIR}include/BALL/${g}/*.iC")
+		file(GLOB_RECURSE FILES RELATIVE ${PROJECT_SOURCE_DIR} "include/BALL/${g}/*.h" "include/BALL/${g}/*.iC")
 
 		set(HEADER_FILES_GROUP)
 		foreach(f ${FILES})
@@ -40,7 +52,7 @@ macro(BALL_MACRO_collect_headers WORKING_DIR LIST_NAME)
 		endforeach()
 
 		# Add header files to global list of header files
-		list(APPEND ${LIST_NAME} ${HEADER_FILES_GROUP})
+		list(APPEND ${HEADER_FILE_LIST} ${HEADER_FILES_GROUP})
 
 		string(REGEX REPLACE "/" "\\\\" S_GROUP ${g})
 		source_group("Header Files\\\\${S_GROUP}" FILES ${HEADER_FILES_GROUP})
@@ -52,7 +64,7 @@ endmacro()
 # --------------------------------------------------------------------------
 # Macro to collect source files (*.C) from source directory
 #
-macro(BALL_MACRO_collect_sources)
+macro(BALL_MACRO_collect_sources SOURCE_FILE_LIST)
 
 	# Step 1: collect sources.cmake files listing all source files
 	file(GLOB_RECURSE SOURCES_CMAKE RELATIVE ${PROJECT_SOURCE_DIR} "*sources.cmake")
@@ -68,7 +80,7 @@ macro(BALL_MACRO_collect_sources)
 		source_group("Source Files\\\\${S_GROUP}" FILES ${SOURCES_LIST})
 
 		foreach(f ${SOURCES_LIST})
-			list(APPEND ball_sources ${group}/${f})
+			list(APPEND ${SOURCE_FILE_LIST} ${group}/${f})
 		endforeach()
 
 		# Collect and process parser/lexer files
@@ -87,54 +99,12 @@ macro(BALL_MACRO_collect_sources)
 			BISON_TARGET(${f}Parser ${PARSERINPUT} ${PARSEROUTPUT} COMPILE_FLAGS "--defines=${PARSERHEADER} -p${PREFIX}")
 			FLEX_TARGET(${f}Lexer ${LEXERINPUT} ${LEXEROUTPUT} COMPILE_FLAGS "-P${PREFIX}")
 			ADD_FLEX_BISON_DEPENDENCY(${f}Lexer ${f}Parser)
-			list(APPEND ball_sources ${PARSERINPUT} ${PARSEROUTPUT} ${LEXERINPUT} ${LEXEROUTPUT})
+			list(APPEND ${SOURCE_FILE_LIST} ${PARSERINPUT} ${PARSEROUTPUT} ${LEXERINPUT} ${LEXEROUTPUT})
 		endforeach()
 
 
 	endforeach()
 
-endmacro()
-
-
-# --------------------------------------------------------------------------
-# Macro to add class tests
-#
-macro(deprec_BALL_MACRO_add_class_test_targets GROUP_PREFIX LINK_LIBRARIES)
-
-	SET(ALL_TESTS "")
-
-	get_cmake_property(cmake_variables VARIABLES)
-	foreach(v ${cmake_variables})
-		if(v MATCHES ${GROUP_PREFIX}*)
-			string(TOUPPER ${v} GROUP)
-			add_custom_target(${GROUP})
-			add_dependencies(${GROUP} ${${v}})
-
-			foreach(test ${${v}})
-				add_executable(${test} source/${test}.C)
-				target_link_libraries(${test} ${LINK_LIBRARIES})
-				set_property(TARGET ${test} PROPERTY FOLDER ${GROUP})
-				set_target_properties(${test} PROPERTIES COMPILE_FLAGS "${Qt5Core_EXECUTABLE_COMPILE_FLAGS}")
-
-				add_test(${test} ${CMAKE_RUNTIME_OUTPUT_DIRECTORY}/${test})
-				list(APPEND ALL_TESTS ${test})
-			endforeach()
-		endif()
-	endforeach()
-
-endmacro()
-
-
-# --------------------------------------------------------------------------
-# Macros to set and reset CXX compiler flags
-#
-macro(BALL_MACRO_cxx_flags_set FLAGS)
-	set(BACKUP_CMAKE_CXX_FLAGS_RELEASE ${CMAKE_CXX_FLAGS_RELEASE})
-	set(CMAKE_CXX_FLAGS_RELEASE ${FLAGS})
-endmacro()
-
-macro(BALL_MACRO_cxx_flags_reset)
-	set(CMAKE_CXX_FLAGS_RELEASE ${BACKUP_CMAKE_CXX_FLAGS_RELEASE})
 endmacro()
 
 
